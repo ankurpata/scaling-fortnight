08fd7ac3b8d92421923f84925eeff595f921b326
Ankur Patankar <ankur.pata@gmail.com>
2020-05-05 16:59:50 +0400

    fix: imoprt

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,3 +1,7 @@
-import schema from "./schema.graphql";
+// import schema from "./schema.graphql";
+import importAsString from "@reactioncommerce/api-utils/importAsString.js";
+
+const schema = importAsString("./schema.graphql");
+
 
 export default [schema];

258e3139cf33872d02b9b505fa1e8d935185b209
Ankur Patankar <ankur.pata@gmail.com>
2020-05-06 11:14:10 +0400

    fix: hellow

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -2,6 +2,7 @@
 import importAsString from "@reactioncommerce/api-utils/importAsString.js";
 
 const schema = importAsString("./schema.graphql");
+const productInput = importAsString("./product-input.graphql");
 
 
-export default [schema];
+export default [schema, productInput];

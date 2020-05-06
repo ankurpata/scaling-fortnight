660aa6e377edc5cb13e6895f6070f949b4177d92
Ankur Patankar <ankur.pata@gmail.com>
2020-05-06 16:25:14 +0400

    fix: fix

---
+++ src/i18n/index.js
@@ -0,0 +1,13 @@
+
+import en from "./en.json";
+
+//
+// we want all the files in individual
+// imports for easier handling by
+// automated translation software
+//
+export default {
+    translations: [
+        ...en
+    ]
+};

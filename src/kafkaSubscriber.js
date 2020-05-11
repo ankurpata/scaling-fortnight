6480e04ca768129630e6978cb5536fd44e2d9359
Ankur Patankar <ankur.pata@gmail.com>
2020-05-11 22:16:08 +0400

    fix: host name error

--- src/kafkaSubscriber.js
+++ src/kafkaSubscriber.js
@@ -5,9 +5,9 @@
 // eslint-disable-next-line node/no-extraneous-import
 import Logger from "@reactioncommerce/logger";
 
-import kafkaApi from "kafka-node";
+import Kafka from "node-rdkafka"; // see: https://github.com/blizzard/node-rdkafka
 
-const {KafkaClient, Consumer} = kafkaApi;
+const CONSUMER_GROUP_ID = "node-consumer-2";
 
 
 /**
@@ -20,7 +20,8 @@ export default async function kafkaSubscriber() {
         console.log("kafka consumer is booting up");
         Logger.info("!!! Connecting Kafka !!!");
 
-        const client = new KafkaClient({
+        const kafkaConf = {
+            "group.id": CONSUMER_GROUP_ID,
             "metadata.broker.list": "rocket-01.srvs.cloudkafka.com:9094,rocket-02.srvs.cloudkafka.com:9094,rocket-03.srvs.cloudkafka.com:9094",
             "security.protocol": "SASL_SSL",
             "sasl.mechanisms": "SCRAM-SHA-256",
@@ -28,34 +29,46 @@ export default async function kafkaSubscriber() {
             "sasl.password": "e8hPouz3LL2rhp_vtQhp547rYsr9BbhQ",
             "socket.keepalive.enable": true,
             "debug": "generic,broker,security"
+        };
+
+        const topics = ["bddcy39c-default"];
+
+
+        var stream = new Kafka.KafkaConsumer.createReadStream(kafkaConf, {"auto.offset.reset": "earliest"}, {
+            topics: topics
         });
-        const consumer = new Consumer(
-            client,
-            [{topic: "bddcy39c-default", partition: 1}],
-            {
-                autoCommit: true,
-                fetchMaxWaitMs: 1000,
-                fetchMaxBytes: 1024 * 1024,
-                encoding: "utf8",
-                fromOffset: false
-            }
-        );
-        consumer.on("message", async (message) => {
-            const consumerdata = JSON.parse(message.value);
+
+        stream.on('data', function (message) {
+            console.log(`Consumed message on Stream: ${message.value.toString()}`);
             console.log(message, "~~~~~Message Received@kafkaConsumerReactionCommerce~~~~~~");
             Logger.info(message);
             Logger.info("~~~~~Message Received@kafkaConsumerReactionCommerce~~~~~~");
-            console.log("===>", consumerdata);
-
-            console.log(typeof (consumerdata.data));
 
             console.log("deleteStatus");
             console.log("Post Deleted Successfully");
+            // the structure of the messages is as follows:
+            //   {
+            //     value: Buffer.from('hi'), // message contents as a Buffer
+            //     size: 2, // size of the message, in bytes
+            //     topic: 'librdtesting-01', // topic the message comes from
+            //     offset: 1337, // offset the message was read from
+            //     partition: 1, // partition the message was on
+            //     key: 'someKey', // key of the message if present
+            //     timestamp: 1510325354780 // timestamp of message creation
+            //   }
+        });
+        console.log(`Stream consumer created to consume from topic ${topics}`);
 
+        stream.consumer.on("disconnected", function (arg) {
+            console.log(`The stream consumer has been disconnected`)
+            process.exit();
         });
-        consumer.on("error", (err) => {
-            console.log("error", err);
-        });
+
+// automatically disconnect the consumer after 30 seconds
+        setTimeout(function () {
+            stream.consumer.disconnect();
+        }, 30000)
+
     } catch (e) {
         console.log(e);
     }
